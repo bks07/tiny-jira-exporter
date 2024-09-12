a2ebdb54ed3ee1a573629600fc1b9f9ed666158d
Boris Karl Schlein <boris.karl.schlein@gmail.com>
2024-09-12 21:25:20 +0200

    Added virtual environment venv/ to .gitignore. Added requirements file for develop branch.

--- tiny-jira-exporter.py
+++ tiny-jira-exporter.py
@@ -9,37 +9,18 @@ from packages.exporter_config.exporter_config import ExporterConfig
 
 DEFAULT_CONFIGURATION_FILE = "./conf/default.yaml"
 DEFAULT_OUTPUT_FILE = "./exports/default.csv"
+DISABLE_LOGGING = -1
 
 def main():
     # Create the parser for handing over arguments
-    parser = argparse.ArgumentParser(description="This script fetches Jira issues with their timestamps given a defined workflow.")
-    parser.add_argument("-c", "--config", type=str, dest="config", help="The configuration input file name. Type must be YAML.")
-    parser.add_argument("-o", "--output", type=str, dest="output", help="The output file name. The output file will be an CSV file.")
-    parser.add_argument("-l", "--loglevel", dest="loglevel", help="d := Debug (most verbose), i := Info, w := Warning, e := Error, c := Critical (least verbose), o := Off (completely disabled)")
-    # Parse the arguments
-    args = parser.parse_args()
+    parser = argparse.ArgumentParser(description = "This script fetches Jira issues with their timestamps given a defined workflow.")
+    parser.add_argument("-c", "--config", type = str, dest ="config", help = "The configuration input file name. Type must be YAML.")
+    parser.add_argument("-o", "--output", type = str, dest ="output", help = "The output file name. The output file will be an CSV file.")
+    parser.add_argument("-l", "--loglevel", type = str, dest ="loglevel", help = "d := Debug (most verbose), i := Info, w := Warning, e := Error, c := Critical (least verbose), o := Off (completely disabled)")
 
     try:
-        logger = logging.getLogger(__name__)
-        log_level = args.loglevel
-        if log_level == "o" or log_level == None:
-            logger.addHandler(logging.NullHandler())
-        else:
-            formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
-            console_handler = logging.StreamHandler()
-            console_handler.setFormatter(formatter)
-            logger.addHandler(console_handler)
-            match log_level:
-                case "d":
-                    console_handler.setLevel(logging.DEBUG) # most verbose
-                case "i":
-                    console_handler.setLevel(logging.INFO)
-                case "w":
-                    console_handler.setLevel(logging.WARNING)
-                case "e":
-                    console_handler.setLevel(logging.ERROR)
-                case "c":
-                    console_handler.setLevel(logging.CRITICAL)
+        # Parse the arguments
+        args = parser.parse_args()
 
         yaml_config_file_location = args.config
         if yaml_config_file_location == None or yaml_config_file_location == "":
@@ -49,22 +30,50 @@ def main():
         if csv_output_file_location == None or csv_output_file_location == "":
             csv_output_file_location = DEFAULT_OUTPUT_FILE
 
+        logging.basicConfig()
+        logger = logging.getLogger(__name__)
+        log_level = DISABLE_LOGGING
+        match str(args.loglevel):
+                case "d":
+                    log_level = logging.DEBUG # most verbose
+                case "i":
+                    log_level = logging.INFO
+                case "w":
+                    log_level = logging.WARNING
+                case "e":
+                    log_level = logging.ERROR
+                case "c":
+                    log_level = logging.CRITICAL # least verbose
+                case _: # "o" for off or anything else
+                    log_level = -1 # don't log anything
+
+        if log_level == DISABLE_LOGGING:
+            logger.addHandler(logging.NullHandler())
+            logger.setLevel(logging.CRITICAL)
+        else:
+            formatter = logging.Formatter('%(asctime)s :: %(levelname)s :: %(message)s')
+            console_handler = logging.StreamHandler()
+            console_handler.setFormatter(formatter)
+            logger.addHandler(console_handler)
+            logger.setLevel(logging.DEBUG)
+
+
         print("Process YAML config file...")
         config = ExporterConfig(yaml_config_file_location, logger)
         print(" ... done.")
 
-        if config.get_domain() == "" or \
-            config.get_username() == "" or \
-            config.get_api_token() == "":
+        if config.domain == "" or \
+            config.username == "" or \
+            config.api_token == "":
             print("\nPlease enter the following connection details manually.")
-            if config.get_domain() == "":
-                config.set_domain(input("Jira domain name (https://[yourname].atlassian.net): "))
+            if config.domain == "":
+                config.domain = str(input("Jira domain name (https://[yourname].atlassian.net): "))
 
-            if config.get_username() == "":
-                config.set_username(input("Enter your Jira username: "))
+            if config.username == "":
+                config.username = str(input("Enter your Jira username: "))
             
-            if config.get_api_token() == "":
-                config.set_api_token(input("Enter your Jira API token: "))
+            if config.api_token == "":
+                config.api_token = str(input("Enter your Jira API token: "))
         
         # Parse all received issues
         print("\nFetch issues from Jira...")
